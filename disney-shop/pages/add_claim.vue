86d6057f0eeaa896d42c5e2ff823d8af84730d1c
thanwaralee <thnawaralee.t@ku.th>
2023-11-04 21:08:10 +0700

    claim front end

---
+++ disney-shop/pages/add_claim.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex flex-col justify-start">
+        <div class="h-screen py-8">
+            <div class="mx-auto px-4">
+                <h1 class="text-[48px] text-[#FFFFFF] font-semibold mb-10 mx-auto w-[358px] bg-[#9985BB] text-center rounded-[20px] shadow-md">Add Claim</h1>
+
+                <MenuLink to="/claim" class="bg-[#9985BB] text-white py-2 px-4 rounded-lg mt-4 w-full text-center">
+                                Submit
+                </MenuLink>
+            
+
+           </div>
+       </div>
+   </div>
+</template>
