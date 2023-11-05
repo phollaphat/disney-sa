45578dab00df0fdb28ccdfc5bae52c6a267922a7
thanwaralee <thnawaralee.t@ku.th>
2023-11-06 05:21:18 +0700

    seed Product

--- example-app/database/seeders/UserSeeder.php
+++ example-app/database/seeders/UserSeeder.php
@@ -14,5 +14,13 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::factory(10)->create();
+
+        $user = new User();
+        $user->name = "admin";
+        $user->email = "admin@example.org";
+        $user->password = "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi";
+        $user->role = "พนักงานขาย";
+        $user->tel = "0841104459";
+        $user->save();
     }
 }
